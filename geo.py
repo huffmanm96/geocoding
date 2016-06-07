64d8ea62e7bcc7fe883788c988d3e774193cb613
huffmanm96 <huffmanm96@gmail.com>
2016-06-07 16:47:44 -0400

    Update geo.py

--- geo.py
+++ geo.py
@@ -2,7 +2,7 @@
 # For each pair, it will check to see if any of the other pairs are within a 50m sqare radius
 # It will then add all of the other pairs in that radius to a list that is named after the main point
 import csv
-import numpy as np
+
 
 data = open('New_Geocodes.csv','r')
 d = csv.reader(data)
@@ -22,14 +22,13 @@ for key in dict:
     new_dict = {}
     for each_key in dict:
         if each_key != current:
-            if each_key in range((key-0.0005),(key+0.0005)) and dict[each_key] in range((dict[key]-0.0005),(dict[key]+0.0005)):
-            #if each_key >=((key-0.0005) and each_key <= (key+0.0005)) and dict[each_key] >= dict[key]-0.0005 and dict[each_key] <= dict[key]+0.0005:
-
+            if (each_key>(key-0.0005) and each_key<(key+0.0005))==True and (dict[each_key]>(dict[key]-0.0005) and dict[each_key]<(dict[key]+0.0005))==True:
                 new_dict[each_key] = dict[each_key]
             else: 
                 pass
         else:
             pass
     vals[(key, dict[key])] = new_dict
+    print ("One down.")
 print (vals)
 
